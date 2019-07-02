5cfca50becdc4833f48c7621429b90efa31781ac
www <www@www.com>
2019-07-03 07:05:56 +0800

    x

---
+++ player/main.c
@@ -0,0 +1,152 @@
+#pragma warning(disable: 4101)
+
+#include <stdlib.h>
+#ifdef _WIN32
+# include <crtdbg.h>
+#endif
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#define HAVE_M_PI
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+#include <screen.h>
+
+
+
+static int mainReturn(int r)
+{
+#if !defined(NDEBUG) && defined(_WIN32)
+    system("pause");
+#endif
+    return r;
+}
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+#if !defined(NDEBUG) && defined(_WIN32)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+    //SDL_GL_SetSwapInterval(0);
+
+    u32 winWidth = 800;
+    u32 winHeight = 600;
+    SDL_Window* window = SDL_CreateWindow
+    (
+        "PLAYER",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winWidth, winHeight,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+
+    bool quit = false;
+    SDL_Event e;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+            {
+                quit = true;
+                break;
+            }
+            default:
+                break;
+            }
+        }
+        SDL_GL_SwapWindow(window);
+        // SDL_Delay(1);
+    }
+
+
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return mainReturn(EXIT_SUCCESS);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ screen_a.h
@@ -0,0 +1,145 @@
+#pragma once
+
+
+
+#include "screen.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+
+
+
+
+#ifdef ARYLEN
+# undef ARYLEN
+#endif
+#define ARYLEN(a) (sizeof(a) / sizeof((a)[0]))
+
+
+
+
+#ifdef max
+# undef max
+#endif
+#ifdef min
+# undef min
+#endif
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) ((a) < (b) ? (a) : (b))
+
+
+
+
+#define zalloc(sz) calloc(1, sz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
