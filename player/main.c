9b29fb458791b417ca5687dcf20d110618b4783e
www <www@www.com>
2019-07-04 20:00:30 +0800

    x

--- player/main.c
+++ player/main.c
@@ -22,6 +22,9 @@
 
 #include <argparse.h>
 #include <fileu.h>
+#include <vec.h>
+
+#include <cJSON.h>
 
 
 
@@ -67,9 +70,12 @@ static int mainReturn(int r)
 
 
 
+
+
 enum
 {
     TimeStrBuf_MAX = 16,
+    PATH_MAX = 260,
 };
 
 static char* nowStr(char* timeBuf)
@@ -83,31 +89,300 @@ static char* nowStr(char* timeBuf)
 
 
 
+
+static u32 loadFileDataToBuf(vec_char* dataBuf, const char* filename)
+{
+    u32 size = FILEU_readFile(filename, NULL, 0);
+    if ((-1 == size) || !size)
+    {
+        // todo report error
+        return -1;
+    }
+    u32 offset = dataBuf->length;
+    vec_resize(dataBuf, dataBuf->length + size + 1);
+    size = FILEU_readFile(filename, dataBuf->data + offset, size);
+    dataBuf->data[offset + size] = 0;
+    return offset;
+}
+
+
+static void loadSceneByJson(const char* code, const char* dir, SCREEN_Scene* desc)
+{
+    vec_char dataBuf[1] = { 0 };
+    char path[PATH_MAX] = "";
+    u32 commShaderOff = -1, imageShaderOff = -1;
+    bool bufferUsed[SCREEN_Buffers_MAX] = { 0 };
+    u32 bufferShaderOff[SCREEN_Buffers_MAX] = { 0 };
+
+    cJSON* root = cJSON_Parse(code);
+    if (!root)
+    {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        // todo report error
+        return;
+    }
+    {
+        cJSON* shader = cJSON_GetObjectItem(root, "commonShader");
+        if (!cJSON_IsString(shader))
+        {
+            // todo report error
+            goto error;
+        }
+        char* filename = cJSON_GetStringValue(shader);
+        snprintf(path, sizeof(path), "%s/%s", dir, filename);
+        u32 off = loadFileDataToBuf(dataBuf, path);
+        if (-1 == off)
+        {
+            // todo report error
+            goto error;
+        }
+        commShaderOff = off;
+    }
+    cJSON* buffers = cJSON_GetObjectItem(root, "buffers");
+    if (!cJSON_IsArray(buffers))
+    {
+        // todo report error
+        goto error;
+    }
+    {
+        cJSON *buffer, *channel;
+        u32 bi = 0;
+        cJSON_ArrayForEach(buffer, buffers)
+        {
+            if (bi >= SCREEN_Buffers_MAX)
+            {
+                // todo report error
+                goto error;
+            }
+            if (!cJSON_IsObject(buffer) && !cJSON_IsNull(buffer))
+            {
+                // todo report error
+                goto error;
+            }
+            if (cJSON_IsNull(buffer))
+            {
+                ++bi;
+                continue;
+            }
+            cJSON* shader = cJSON_GetObjectItem(buffer, "shader");
+            if (!cJSON_IsString(shader))
+            {
+                // todo report error
+                goto error;
+            }
+            char* filename = cJSON_GetStringValue(shader);
+            snprintf(path, sizeof(path), "%s/%s", dir, filename);
+            u32 off = loadFileDataToBuf(dataBuf, path);
+            if (-1 == off)
+            {
+                // todo report error
+                goto error;
+            }
+            bufferShaderOff[bi] = off;
+            bufferUsed[bi] = true;
+
+            cJSON* channels = cJSON_GetObjectItem(buffer, "channels");
+            if (!cJSON_IsArray(channels))
+            {
+                // todo report error
+                goto error;
+            }
+            u32 ci = 0;
+            cJSON_ArrayForEach(channel, channels)
+            {
+                if (ci >= SCREEN_Channels_MAX)
+                {
+                    // todo report error
+                    goto error;
+                }
+                if (!cJSON_IsObject(channel) && !cJSON_IsNull(channel))
+                {
+                    // todo report error
+                    goto error;
+                }
+                if (cJSON_IsNull(channel))
+                {
+                    ++ci;
+                    continue;
+                }
+                cJSON* type = cJSON_GetObjectItem(channel, "type");
+                if (!cJSON_IsString(type))
+                {
+                    // todo report error
+                    goto error;
+                }
+                char* typeStr = cJSON_GetStringValue(type);
+                if (0 == strcicmp(typeStr, "buffer"))
+                {
+                    desc->buffer[bi].channel[ci].type = SCREEN_ChannelType_Buffer;
+
+                    cJSON* bufferId = cJSON_GetObjectItem(channel, "buffer");
+                    if (!cJSON_IsNumber(bufferId))
+                    {
+                        // todo report error
+                        goto error;
+                    }
+                    u32 id = bufferId->valueint;
+                    desc->buffer[bi].channel[ci].buffer = id;
+                }
+                else
+                {
+                    // todo report error
+                    goto error;
+                }
+                ++ci;
+            }
+            ++bi;
+        }
+    }
+
+    cJSON* image = cJSON_GetObjectItem(root, "image");
+    if (!cJSON_IsObject(image))
+    {
+        // todo report error
+        goto error;
+    }
+    {
+        cJSON* shader = cJSON_GetObjectItem(image, "shader");
+        if (!cJSON_IsString(shader))
+        {
+            // todo report error
+            goto error;
+        }
+        char* filename = cJSON_GetStringValue(shader);
+        snprintf(path, sizeof(path), "%s/%s", dir, filename);
+        u32 off = loadFileDataToBuf(dataBuf, path);
+        if (-1 == off)
+        {
+            // todo report error
+            goto error;
+        }
+        imageShaderOff = off;
+
+        cJSON* channels = cJSON_GetObjectItem(image, "channels");
+        if (!cJSON_IsArray(channels))
+        {
+            // todo report error
+            goto error;
+        }
+        cJSON* channel;
+        u32 ci = 0;
+        cJSON_ArrayForEach(channel, channels)
+        {
+            if (ci >= SCREEN_Channels_MAX)
+            {
+                // todo report error
+                goto error;
+            }
+            if (!cJSON_IsObject(channel) && !cJSON_IsNull(channel))
+            {
+                // todo report error
+                goto error;
+            }
+            if (cJSON_IsNull(channel))
+            {
+                ++ci;
+                continue;
+            }
+
+            cJSON* type = cJSON_GetObjectItem(channel, "type");
+            if (!cJSON_IsString(type))
+            {
+                // todo report error
+                goto error;
+            }
+            char* typeStr = cJSON_GetStringValue(type);
+            if (0 == strcicmp(typeStr, "buffer"))
+            {
+                desc->image.channel[ci].type = SCREEN_ChannelType_Buffer;
+
+                cJSON* bufferId = cJSON_GetObjectItem(channel, "buffer");
+                if (!cJSON_IsNumber(bufferId))
+                {
+                    // todo report error
+                    goto error;
+                }
+                u32 id = bufferId->valueint;
+                desc->image.channel[ci].buffer = id;
+            }
+            else
+            {
+                // todo report error
+                goto error;
+            }
+            ++ci;
+        }
+    }
+    if (commShaderOff != -1)
+    {
+        desc->shaderComm = dataBuf->data + commShaderOff;
+    }
+    for (u32 i = 0; i < SCREEN_Buffers_MAX; ++i)
+    {
+        if (bufferUsed[i])
+        {
+            desc->buffer[i].shaderCode = dataBuf->data + bufferShaderOff[i];
+        }
+    }
+    desc->image.shaderCode = dataBuf->data + imageShaderOff;
+
+    SCREEN_loadScene(desc);
+
+    vec_free(dataBuf);
+    cJSON_Delete(root);
+    return;
+error:
+    vec_free(dataBuf);
+    cJSON_Delete(root);
+}
+
+
+
 static void loadSceneByFile(const char* filename)
 {
-    SCREEN_Scene desc = { 0 };
+    SCREEN_Scene desc[1] = { 0 };
     if (FILEU_fileExist(filename))
     {
         if (0 == strcicmp(FILEU_filenameExt(filename), "frag"))
         {
-            char* src;
-            u32 srcSize = FILEU_readFile(filename, &src);
-            if ((-1 == srcSize) || !srcSize)
+            u32 size = FILEU_readFile(filename, NULL, 0);
+            if ((-1 == size) || !size)
             {
+                // todo report error
                 return;
             }
-            desc.image.shaderCode = src;
-            SCREEN_loadScene(&desc);
-            free(src);
+            char* buf = malloc(size + 1);
+            size = FILEU_readFile(filename, buf, size);
+            buf[size] = 0;
+            desc->image.shaderCode = buf;
+            SCREEN_loadScene(desc);
+            free(buf);
         }
         else if (0 == strcicmp(FILEU_filenameExt(filename), "json"))
         {
+            u32 size = FILEU_readFile(filename, NULL, 0);
+            if ((-1 == size) || !size)
+            {
+                // todo report error
+                return;
+            }
+            char* buf = malloc(size + 1);
+            size = FILEU_readFile(filename, buf, size);
+            buf[size] = 0;
 
+            char dir[PATH_MAX];
+            FILEU_getDirName(dir, filename, sizeof(dir));
+            loadSceneByJson(buf, dir, desc);
+            free(buf);
         }
     }
     else if (FILEU_dirExist(filename))
     {
         // todo
+    }
+    else
+    {
+        // todo report error
     }
 }
 
@@ -322,6 +597,44 @@ int main(int argc, char* argv[])
 
     return mainReturn(EXIT_SUCCESS);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
