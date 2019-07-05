fe43d6000ddd5636cc9eaa342288bcaf2b0e6c86
www <www@www.com>
2019-07-05 14:17:11 +0800

    x

--- screen.c
+++ screen.c
@@ -106,13 +106,13 @@ static void SCREEN_bufferRunResize(SCREEN_BufferRun* b, const SCREEN_Buffer* des
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, b->texture);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA32F, ctx->width, ctx->height);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA32F, ctx->width, ctx->height);
 }
 
 
@@ -245,6 +245,10 @@ void SCREEN_resize(u32 w, u32 h)
     {
         return;
     }
+    SCREEN_leave();
+    SCREEN_enter(w, h);
+    return;
+
     if (!ctx->width || !ctx->height)
     {
         ctx->pointX = w / 2;
@@ -332,6 +336,8 @@ static void SCREEN_bufferRunRender(SCREEN_BufferRun* b, SCREEN_Buffer* desc)
     {
         if (SCREEN_ChannelType_Unused == desc->channel[i].type)
         {
+            glActiveTexture(GL_TEXTURE0 + i);
+            glBindTexture(GL_TEXTURE_2D, 0);
             continue;
         }
         assert(SCREEN_ChannelType_Buffer == desc->channel[i].type);
