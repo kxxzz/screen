8489d3a0e3b90236ecc94fda0a8ff9da541cc0c7
www <www@www.com>
2019-08-06 19:55:32 +0800

    x

--- screen.c
+++ screen.c
@@ -737,17 +737,19 @@ void SCREEN_enter(u32 w, u32 h)
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+
     glGenVertexArrays(1, &ctx->va);
     glBindVertexArray(ctx->va);
+    glBindBuffer(GL_ARRAY_BUFFER, vb);
     {
-        glBindBuffer(GL_ARRAY_BUFFER, vb);
         const GLint attrib_position = 0;
         glVertexAttribPointer(attrib_position, 2, GL_FLOAT, GL_FALSE, 0, 0);
         glEnableVertexAttribArray(attrib_position);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     SCREEN_GL_CHECK();
+
 
     glDeleteBuffers(1, &vb);
     glBindVertexArray(ctx->va);
